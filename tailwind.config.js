18e92a7fead011e7c78160aedcde75ba512d82f6
ibrqp <mibrahimalamin98@gmail.com>
2025-12-31 04:50:12 +0700

    Menambahkan Font SF Pro Display

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["SF Pro Display", "sans-serif"],
+      },
+    },
+  },
+}
