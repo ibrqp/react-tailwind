5d0e4385927ba97ec9ee18ba8e41da4af34cd06d
ibrqp <mibrahimalamin98@gmail.com>
2025-12-31 04:52:11 +0700

    -

---
+++ src/data/data.js
@@ -0,0 +1,63 @@
+import gambar1 from "../assets/unnamed.jpg"
+import gambar2 from "../assets/unnamed.jpg"
+import gambar3 from "../assets/unnamed.jpg"
+import gambar4 from "../assets/unnamed.jpg"
+import gambar5 from "../assets/unnamed.jpg"
+import gambar6 from "../assets/unnamed.jpg"
+
+
+export const listProyek = [
+    {
+        id: "1",
+        nama: "Beach",
+        gambar: gambar1,
+        kategori: "Pantai",
+        pemilik: "Nesabatechno",
+        tahun: "2025",
+        link: "gatau",
+        deskripsi: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi beatae molestias nulla corporis sunt consequuntur esse est eaque modi consequatur nobis necessitatibus tempore iste sit perspiciatis voluptas magnam nesciunt, sapiente cum corrupti! Perspiciatis est ex voluptates mollitia nisi. Placeat magni veritatis accusantium in quas, officiis, obcaecati nulla velit, modi fugit ad! Incidunt vel distinctio beatae neque aliquam laboriosam dolorum dicta earum nobis possimus recusandae deleniti, iste omnis molestiae, delectus accusantium expedita eius repellendus doloremque repellat? Placeat perferendis in labore velit laborum architecto, porro inventore delectus ipsum sunt aliquid voluptate accusantium consequatur nostrum quaerat quas. Vel exercitationem officia perferendis quae at!",
+        dad: "200"
+    },
+    {
+        id: "2",
+        gambar: gambar2,
+        nama: "-",
+        kategori: "gatau",
+        link: "gatau",
+        dad: "300"
+    },
+    {
+        id: "3",
+        gambar: gambar3,
+        nama: "-",
+        kategori: "gatau",
+        link: "gatau",
+        dad: "400"
+    },
+    {
+        id: "4",
+        gambar: gambar4,
+        nama: "-",
+        kategori: "gatau",
+        link: "gatau",
+        dad: "500"
+    },
+    {
+        id: "5",
+        gambar: gambar5,
+        nama: "-",
+        kategori: "gatau",
+        link: "gatau",
+        dad: "600"
+    },
+    {
+        id: "6",
+        gambar: gambar6,
+        nama: "-",
+        kategori: "gatau",
+        link: "gatau",
+        dad: "700"
+    }
+]
+
+export default listProyek;
