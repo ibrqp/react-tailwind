0410ba6a9d3940e774f435a0251c8b611d654cd3
ibrqp <mibrahimalamin98@gmail.com>
2025-12-30 23:55:43 +0700

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import ContactMe from "./components/ContactMe"
 import Home from "./components/Home"
 import Navbar from "./components/Navbar"
+import Sket from "./components/Sket"
 function App() {
 
   return (
     <>
         <Navbar/>
         <Home/>
+        <Sket/>
+        <ContactMe/>
     </>
   )
 }
